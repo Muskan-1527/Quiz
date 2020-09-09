487f84fd83242380ecda9c6a96f2198a20e518d5
Muskan-1527 <agrawalm883@gmail.com>
2020-09-09 13:58:15 +0530

    scroll to top

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ form.addEventListener('submit', e => {
   });
 
   // show results on page
+  scrollTo(0,0);
   result.querySelector('span').textContent = `${score}%`;
   result.classList.remove('d-none');
 
